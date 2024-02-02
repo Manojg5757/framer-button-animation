21c0c1a23339022a8e67041c84cdfe3f33450831
manojvsp <gmanojece2021@gmail.com>
2024-02-02 13:57:21 +0530

    files changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import React from "react";
-import { motion,MotionConfig } from "framer-motion";
+import React from 'react'
+import ButtonAnimation from './components/ButtonAnimations/ButtonAnimation'
 
 const App = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "20px",
-      }}
-    >
-      <MotionConfig transition={{duration:1}}>
-      <motion.button 
-      className="button1"
-       whileTap={{scale:2.5}}
-       whileHover={{scale:1.5,rotate:'20deg',zIndex:'1000',y:'200px'}}
-      >
-        Hello Wandering Coders
-      </motion.button>
-      <motion.button  
-      className="button2"
-      whileTap={{scale:2.5}}
-       whileHover={{scale:1.5,rotate:'-20deg',y:'200px'}}>
-        Hellow Wanderers
-      </motion.button>
-      </MotionConfig>
+    <div>
+      <ButtonAnimation />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/ButtonAnimations/ButtonAnimation.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './ButtonAnimation.css'
+import {motion,MotionConfig} from 'framer-motion'
+
+const ButtonAnimation = () => {
+    return (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
+          <MotionConfig transition={{duration:1}}>
+          <motion.button 
+          className="button1"
+           whileTap={{scale:2.5}}
+           whileHover={{scale:1.5,rotate:'20deg',zIndex:'1000',y:'200px'}}
+          >
+            Hello Wandering Coders
+          </motion.button>
+          <motion.button  
+          className="button2"
+          whileTap={{scale:2.5}}
+           whileHover={{scale:1.5,rotate:'-20deg',y:'200px'}}>
+            Hellow Wanderers
+          </motion.button>
+          </MotionConfig>
+        </div>
+      );
+}
+
+export default ButtonAnimation
